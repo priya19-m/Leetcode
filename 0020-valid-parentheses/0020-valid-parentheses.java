86b1ce4a54c90d731f7050dd72c8375a61b8558e
priya19-m <mpriyadharshini.141@gmail.com>
2026-07-29 09:27:19 +0530

    Time: 3 ms (86.17%), Space: 43.2 MB (58.04%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch=='(' || ch == '[' || ch =='{'){
+                st.push(ch);
+            }else{
+                if(!st.isEmpty()){
+                    char top=st.peek();
+                    if((ch == ')'&& top=='(')||
+                    (ch==']'&&top=='[')||
+                    (ch=='}'&&top=='{')){
+                        st.pop();
+                    }else{
+                        return false;
+                    }
+                }else{
+                    return false;
+                }
+                
+            }
+        }
+        return st.isEmpty();
+    }
+}
